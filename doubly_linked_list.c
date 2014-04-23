70b735da660428910d13817869291f0af741d5d4
Guilherme <guijocargo@live.com>
2014-04-23 00:34:33 -0300

    Update doubly_linked_list.c

--- doubly_linked_list.c
+++ doubly_linked_list.c
@@ -1,32 +1,33 @@
 /* ========================================================================
- * Copyright 2014 Guilherme J. C. Gois
+ * Copyright 2014 Guilherme J. C. Gois & Felipe L Lara
  * Licensed under MIT (https://github.com/guilhermegois/gois_lib/master/LICENSE)
  * ======================================================================== */
  
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "doubly_linked_list.h"
 
 void startList(Doubly_Linked_List* list) {
-	list = (Doubly_Linked_List *) malloc(sizeof(Doubly_Linked_List));
-	list->first = NULL;
-	list->last = NULL;
+	*list = NULL;
 }
 
 void endList(Doubly_Linked_List* list) {
-	Node *node = list->first;
+	/*Node *node = list->first;
 	while ((node = node->next) != NULL) {
 		free(node->prev);
 	}
 	free(node);
-	free(list);
+	free(list);*/
 }
 
 void insertAtBeginning(Doubly_Linked_List* list, elem_t elem) {
-	Node *newNode = createNode(elem);
-	newNode->next = list->first;
-	list->first->prev = newNode;
-	list->first = newNode;
+	Node *newNode;
+	newNode = createNode(elem);
+	newNode->next = *list;
+	if (!isEmpty(list))
+		(*list)->prev = newNode;
+	*list = newNode;
 }
 
 void insertAtPrevious(Doubly_Linked_List* list, Node newNode, elem_t elem) {	}
@@ -34,23 +35,22 @@ void insertAtPrevious(Doubly_Linked_List* list, Node newNode, elem_t elem) {	}
 void insertAtNext(Doubly_Linked_List* list, Node newNode, elem_t elem) {	}
 
 void insertAtLast(Doubly_Linked_List* list, elem_t elem) {
-	Node *newNode = createNode(elem);
-	if (list->last != NULL) {
-		newNode->prev = list->last;
-		list->last->next = newNode;
-		list->last = newNode;
+	Node *newNode, *aux;
+	newNode = createNode(elem);
+	if (isEmpty(list)) {
+		insertAtBeginning(list, elem);		
 	} else {
-		insertAtBeginning(list, elem);
+		aux = *list;
+		while (aux->next != NULL)
+			aux = aux->next;
+		newNode->next = aux->next;
+		aux->next = newNode;
+		newNode->prev = aux;
 	}
 }
 
 int deleteAtBeginning(Doubly_Linked_List* list) {
-	if (list == NULL)
-		return 0;
-	
-	list->first = list->first->next;
-	free(list->first->prev);
-	return 1;
+	return 0;
 }
 
 int deleteAtPrevious(Doubly_Linked_List* list, Node node) {
@@ -62,16 +62,12 @@ int deleteAtNext(Doubly_Linked_List* list, Node node) {
 }
 
 int deleteAtLast(Doubly_Linked_List* list) {
-	if (list == NULL)
-		return 0;
-	
-	list->last = list->last->prev;
-	free(list->last->next);
-	return 1;
+	return 0;
 }
 
 Node* createNode(int elem) {
-	Node* newNode = (Node *) malloc(sizeof(Node));
+	Node* newNode;
+	newNode = malloc(sizeof(Node));
 	
 	newNode->info = elem;
 	
@@ -79,15 +75,15 @@ Node* createNode(int elem) {
 }
 
 int isEmpty(Doubly_Linked_List* list) {
-	if (list->first == NULL && list->last == NULL)
+	if (*list == NULL)
 		return 1;
 	return 0;
 }
 
-void print(Doubly_Linked_List list) {
-	Node *node = list.first;
+void print(Doubly_Linked_List* list) {
+	Node *node = *list;
 	while (node != NULL) {
-		printf("%d", node->info);
+		printf("%d ", node->info);
 		node = node->next;
 	}
 }
